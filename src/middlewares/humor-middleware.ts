cfcc0326c118683aaa6ddda0437215d9c6d330a6
manuelamp <manuella.m.p182@gmail.com>
2023-01-20 20:30:47 -0300

    feat: middlewares created

--- src/middlewares/humor-middleware.ts
+++ src/middlewares/humor-middleware.ts
@@ -1,1 +1,34 @@
+import express, { Request, Response, NextFunction } from "express";
 import { humorAnalysisPost } from "../schemas/humor-schema.js";
+import { selectHumorById } from "../repositories/humor-repository.js";
+
+export function postSchemaValidation(req: Request, res: Response, next: NextFunction) {
+
+    const validationStatus = humorAnalysisPost.validate(req.body, { abortEarly: false });
+
+    if (validationStatus.error) {
+        const error = validationStatus.error.details.map((detail) => detail.message);
+        res.status(400).send(error);
+        return;
+    };
+
+    next();
+
+}
+
+export async function humorExistenceValidation(req: Request, res: Response, next: NextFunction) {
+
+    const { id } = req.params;
+
+    try {
+        const isHumorExists = await selectHumorById(id);
+
+        if(isHumorExists.rows.length === 0){
+            return res.sendStatus(404);
+        }
+
+    } catch (error) {
+        return res.status(500).send({message: error.message});
+    }
+
+}
--- src/routers/humor-router.ts
+++ src/routers/humor-router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getHumors, postHumor, putHumor, deleteHumor } from "../controllers/humor-controller.js";
+import { postSchemaValidation, humorExistenceValidation } from "../middlewares/humor-middleware.js";
 
 const router = Router();
 
 router.get("/humors", getHumors);
-router.post("/humor", postHumor);
-router.put("/humor/:id", putHumor);
-router.delete("/humor/:id", deleteHumor);
+router.post("/humor", postSchemaValidation, postHumor);
+router.put("/humor/:id", humorExistenceValidation, putHumor);
+router.delete("/humor/:id", humorExistenceValidation, deleteHumor);
 
 export default router;
