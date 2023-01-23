344ca87411bd2ecf1bbaa4597069a1fdb2f62177
manuelamp <manuella.m.p182@gmail.com>
2023-01-23 14:34:38 -0300

    feat: read  me

---
+++ src/README.md
@@ -0,0 +1,74 @@
+## Humor Analysis
+
+Application to record your mood and activities.
+
+## How to run for development
+
+1. Clone this repository
+
+2. Install all dependencies:
+
+```bash
+npm i
+```
+
+3. Configure the `.env` file using the `.env.example` file
+
+4. Run the back-end in a development environment:
+
+```bash
+npm run dev
+```
+
+## Documentation
+
+- **GET**/humors
+
+Get all your records or filter by mood.
+
+Response:
+
+```bash
+{
+    "id": 1,
+    "date": "21/01/2022",
+    "hour": "20:56",
+    "mood": "bem",
+    "activities": "almoço com amigos"
+}
+```
+
+- **POST**/humor
+
+Body: 
+
+```bash
+{
+    "date": "21/01/2022",
+    "hour": "20:56",
+    "mood": "bem",
+    "activities": "almoço com amigos"
+}
+```
+
+- **PUT**/humor/:id
+
+Update your record.
+
+Body:
+
+```bash
+{
+    "mood": "bem",
+    "activities": "almoço com amigos e praia"
+}
+```
+
+- **DELETE**/humor/:id
+
+Delete your record.
+
+
+
+
+
--- src/repositories/humor-repository.ts
+++ src/repositories/humor-repository.ts
@@ -10,7 +10,7 @@ export async function selectHumorsByMood(mood: string): Promise<QueryResult<humo
     return connectionDB.query(`SELECT * FROM humor WHERE LOWER (mood) LIKE $1;`, [mood + "%"]);
 }
 
-export async function insertHumor(humor: humorPost): Promise<QueryResult> {
+export async function insertHumor(humor: humorPost) {
     return connectionDB.query(`INSERT INTO humor (date, hour, mood, activities) VALUES ($1, $2, $3, $4);`,
         [humor.date, humor.hour, humor.mood, humor.activities]);
 }
@@ -19,10 +19,10 @@ export async function selectHumorById(id: number | string): Promise<QueryResult<
     return connectionDB.query(`SELECT * FROM humor WHERE id=$1;`, [id]);
 }
 
-export async function updateHumor(humor: humorUpdate, id: number | string): Promise<QueryResult> {
+export async function updateHumor(humor: humorUpdate, id: number | string) {
     return connectionDB.query(`UPDATE humor SET mood=$1, activities=$2 WHERE id=$3;`, [humor.mood, humor.activities, id]);
 }
 
-export async function removeHumor(id: number | string): Promise<QueryResult> {
+export async function removeHumor(id: number | string) {
     return connectionDB.query(`DELETE from humor WHERE id=$1;`, [id]);
 }
